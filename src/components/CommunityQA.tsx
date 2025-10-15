e320d4b0ccb256dff29b7800034a43961c033c43
StudyHiveHub <IronSamurai4793@outlook.com>
2025-10-15 06:46:39 -0500

    Create CommunityQA.tsx

---
+++ src/components/CommunityQA.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function CommunityQA() {
+  return (
+    <section>
+      <h2>Community Q&A & Collaboration</h2>
+      <p>Post tricky problems, vote on solutions, and join study squads!</p>
+      {/* Add Q&A board, voting, and squad features here */}
+    </section>
+  );
+}
+
+export default CommunityQA;
