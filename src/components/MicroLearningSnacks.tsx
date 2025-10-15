8bf178fe359b68e7f3b37c09cdb328fbfa314009
StudyHiveHub <IronSamurai4793@outlook.com>
2025-10-15 06:45:22 -0500

    Create MicroLearningSnacks.tsx

---
+++ src/components/MicroLearningSnacks.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function MicroLearningSnacks() {
+  return (
+    <section>
+      <h2>Micro-Learning Snacks</h2>
+      <p>Quick, 5-minute lessons and challenges to keep your brain active!</p>
+      {/* Add snack challenges and lesson cards here */}
+    </section>
+  );
+}
+
+export default MicroLearningSnacks;
